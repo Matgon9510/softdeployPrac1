2c6607bd8747890da59a17c68c3cee267d0fd982
Lukas-jb <lukasjimenez297311@correo.itm.edu.co>
2026-02-24 21:54:39 -0500

    cambio en el retorno del servicio home

--- softdeployPrac1/Controllers/HomeController.cs
+++ softdeployPrac1/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace softdeployPrac1.Controllers
         [HttpGet]
         public IActionResult GetMessage()
         {
-            return Ok("Hola mundo, validando conflictos");
+            return Ok("Hola Despliegue, validando conflictos desde VS");
         }
     }
 }
